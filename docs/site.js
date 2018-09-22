2add11ea9b6bca1a2d64885196bda9092dbc9776
Jennings Anderson <jennings.anderson@gmail.com>
2018-09-21 21:21:37 -0600

    too much fun

--- docs/site.js
+++ docs/site.js
@@ -3,29 +3,124 @@ var data = {
             type: "Feature",
             properties: {
                 name: "Maptime Milehigh",
-                location: "Auraria Library"
+                location: "Auraria Library",
+                rsvp: "#",
+                zoom: {center:[-105.00230,39.75],zoom:13}
             },
             geometry:{
-                type:"Point",
-                coordinates:[-105.002931,39.743355]
-            }
-        },
+                "type": "Polygon",
+                "coordinates": [
+                  [
+                    [
+                      -105.0036386,
+                      39.7435861
+                    ],
+                    [
+                      -105.0030392,
+                      39.7427925
+                    ],
+                    [
+                      -105.0022031,
+                      39.7431659
+                    ],
+                    [
+                      -105.0028026,
+                      39.7439595
+                    ],
+                    [
+                      -105.0036386,
+                      39.7435861
+                    ]
+                  ],
+                  [
+                    [
+                      -105.0030034,
+                      39.7435723
+                    ],
+                    [
+                      -105.0028304,
+                      39.7436511
+                    ],
+                    [
+                      -105.0027264,
+                      39.743516
+                    ],
+                    [
+                      -105.0028993,
+                      39.7434372
+                    ],
+                    [
+                      -105.0030034,
+                      39.7435723
+                    ]
+                  ],
+                  [
+                    [
+                      -105.0030624,
+                      39.7431655
+                    ],
+                    [
+                      -105.0027948,
+                      39.7432842
+                    ],
+                    [
+                      -105.0026931,
+                      39.7431486
+                    ],
+                    [
+                      -105.0029606,
+                      39.7430299
+                    ],
+                    [
+                      -105.0030624,
+                      39.7431655
+                    ]
+                ]
+            ]
+        }
+    },
     "2" : {
             type: "Feature",
             properties: {
                 name: "OSM Colorado",
-                location: "Woods Boss Pizza"
+                location: "Woods Boss Pizza",
+                rsvp: "https://www.meetup.com/OSM-Colorado/events/254808409/",
+                zoom: {center:[-104.983,39.759],zoom:13},
             },
             geometry:{
-                type:"Point",
-                coordinates:[-104.9867597,39.750958]
+                "type": "Polygon",
+                "coordinates": [
+                  [
+                    [
+                      -104.9846217,
+                      39.7508863
+                    ],
+                    [
+                      -104.9843078,
+                      39.7506403
+                    ],
+                    [
+                      -104.9841839,
+                      39.7507337
+                    ],
+                    [
+                      -104.9844977,
+                      39.7509797
+                    ],
+                    [
+                      -104.9846217,
+                      39.7508863
+                    ]
+                  ]
+                ]
             }
         },
     "3" : {
             type: "Feature",
             properties: {
                 name: "Englewood",
-                location: "Somewhere"
+                location: "Somewhere",
+                rsvp: ""
             },
             geometry:{
                 type:"Point",
@@ -36,7 +131,8 @@ var data = {
             type: "Feature",
             properties: {
                 name: "Fort Collins",
-                location: "Library"
+                location: "Library",
+                rsvp: ""
             },
             geometry:{
                 type:"Point",
@@ -47,20 +143,201 @@ var data = {
             type: "Feature",
             properties: {
                 "name":"Maptime Boulder",
-                "location":"Boulder Library"
+                "location":"Boulder Library",
+                rsvp: "https://www.meetup.com/Maptime-Boulder/events/254667358/",
+                zoom: {'center':[-105.27946,40.022],'zoom':13}
             },
             geometry:{
-                type:"Point",
-                coordinates:[-105.2754031,39.9991963]
-            }
+                "type": "Polygon",
+                "coordinates": [
+                  [
+                    [
+                      -105.2812398,
+                      40.0138983
+                    ],
+                    [
+                      -105.2813718,
+                      40.0138667
+                    ],
+                    [
+                      -105.2814057,
+                      40.0139145
+                    ],
+                    [
+                      -105.2814546,
+                      40.0139594
+                    ],
+                    [
+                      -105.2815142,
+                      40.0139961
+                    ],
+                    [
+                      -105.2815821,
+                      40.014023
+                    ],
+                    [
+                      -105.2816555,
+                      40.0140391
+                    ],
+                    [
+                      -105.2817317,
+                      40.0140438
+                    ],
+                    [
+                      -105.2818076,
+                      40.0140369
+                    ],
+                    [
+                      -105.2818426,
+                      40.0141378
+                    ],
+                    [
+                      -105.2817904,
+                      40.0141484
+                    ],
+                    [
+                      -105.2818634,
+                      40.0143586
+                    ],
+                    [
+                      -105.2820411,
+                      40.0143224
+                    ],
+                    [
+                      -105.2821065,
+                      40.0145105
+                    ],
+                    [
+                      -105.2821448,
+                      40.0146208
+                    ],
+                    [
+                      -105.2819189,
+                      40.0146669
+                    ],
+                    [
+                      -105.281998,
+                      40.0148946
+                    ],
+                    [
+                      -105.2820698,
+                      40.0151012
+                    ],
+                    [
+                      -105.2825445,
+                      40.0150044
+                    ],
+                    [
+                      -105.2824998,
+                      40.0148757
+                    ],
+                    [
+                      -105.2826621,
+                      40.0148426
+                    ],
+                    [
+                      -105.2826038,
+                      40.014675
+                    ],
+                    [
+                      -105.2824407,
+                      40.0147082
+                    ],
+                    [
+                      -105.2823973,
+                      40.0145832
+                    ],
+                    [
+                      -105.2821794,
+                      40.0146276
+                    ],
+                    [
+                      -105.282161,
+                      40.0145746
+                    ],
+                    [
+                      -105.2822043,
+                      40.0145657
+                    ],
+                    [
+                      -105.2821924,
+                      40.0145316
+                    ],
+                    [
+                      -105.2821767,
+                      40.0144865
+                    ],
+                    [
+                      -105.2821116,
+                      40.014299
+                    ],
+                    [
+                      -105.2821513,
+                      40.0142909
+                    ],
+                    [
+                      -105.2820921,
+                      40.0141205
+                    ],
+                    [
+                      -105.2820645,
+                      40.0141261
+                    ],
+                    [
+                      -105.2820533,
+                      40.014094
+                    ],
+                    [
+                      -105.2820224,
+                      40.0141003
+                    ],
+                    [
+                      -105.2819922,
+                      40.0140133
+                    ],
+                    [
+                      -105.2820177,
+                      40.0140082
+                    ],
+                    [
+                      -105.2818883,
+                      40.013636
+                    ],
+                    [
+                      -105.2813277,
+                      40.0137485
+                    ],
+                    [
+                      -105.2813324,
+                      40.0137629
+                    ],
+                    [
+                      -105.2812636,
+                      40.0137785
+                    ],
+                    [
+                      -105.281197,
+                      40.0137937
+                    ],
+                    [
+                      -105.2812178,
+                      40.0138445
+                    ],
+                    [
+                      -105.2812398,
+                      40.0138983
+                    ]
+                ]
+            ]
         }
     }
+}
 
 var events = document.getElementsByClassName('event')
 
 for (var i=0; i<events.length; i++){
     events[i].addEventListener('mouseup',function(){
-        updateMap(getFeature(whatData(), this.dataset.locid))
+        updateMap(getFeature(whatData('polygons'), this.dataset.locid),this.dataset.locid)
     })
 }
 
@@ -69,15 +346,29 @@ function getFeature(data, id){
 }
 
 
-function updateMap(feature){
+function updateMap(feature,id){
     if(feature){
         console.log("Moving the map to this feature")
-        console.log(JSON.stringify(feature))
+        console.log(feature.properties.zoom)
 
-        //create a marker popup
-        map.flyTo({center: feature.geometry.coordinates, zoom: 14});
+        Object.keys(markers).forEach(function(key){
+            if(key==id){
+                setTimeout(function(){
+                    markers[id].el.style.display='none';
+                },1500);
+            }else{
+                markers[id].el.style.display='block'
+            }
+        });
 
-        //put it where it belongs
+        popup.setLngLat(turf.center(feature.geometry).geometry.coordinates)
+        
+            .setHTML('<h3 class="txt-h3">' + feature.properties.name + '</h3>'+
+                '<h5 class="txt-h5">' + feature.properties.location + '</h5>'+
+                '<a class="btn round bg-orange-light mt12 mx6 event-link link color-white" href="' + feature.properties.rsvp + '">RSVP</a>')
+            .addTo(map)
+
+        map.flyTo(feature.properties.zoom);
     }
 }
 
@@ -89,42 +380,68 @@ function getFeatureCollection(data){
     return featColl
 }
 
-function whatData(){
-    return data
+function whatData(type){
+    if (type==='polygons') return data;
+    else if (type=='points'){
+        var newData = {}
+        Object.keys(data).forEach(function(key){
+            var p = turf.center(data[key]);
+            newData[key] = {
+                'type':'Feature',
+                'properties':data[key].properties,
+                'geometry':p.geometry}
+        });
+        return newData;
+    }
 }
 
-var feats = getFeatureCollection(whatData())
-
-console.log("Here's where we'd build a map with "+feats.features.length + " features.")
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVubmluZ3NhbmRlcnNvbiIsImEiOiIzMHZndnpvIn0.PS-j7fRK3HGU7IE8rbLT9A';
+
+markers = {}
 
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v9',
-    center: [-105, 39.86],
-    zoom: 8
+    center: [-105.3045, 40.17],
+    zoom: 7
     // hash:true
 });
 
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-right');
 
+var popup = new mapboxgl.Popup({closeOnClick: false})
+    .setLngLat([0,0])
+    .addTo(map)
+
 map.on('load', function () {
 
     map.addLayer({
-        "id": "points",
-        "type": "symbol",
+        "id": "polies",
+        "type": "fill",
         "source": {
             "type": "geojson",
-            "data": getFeatureCollection(whatData())
+            "data": getFeatureCollection(whatData('polygons'))
         },
-        "layout": {
-            "icon-image": "monument-15",
-            "text-field": "{name}",
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
+        "paint": {
+            "fill-color":'salmon',
+            "fill-opacity":0.9,
+            "fill-outline-color":'salmon'
         }
     });
+
+    var d = whatData('points')
+    Object.keys(d).forEach(function(key){
+        var el = document.createElement('div');
+        el.className = 'marker'
+        el.addEventListener('click',function(m){
+            updateMap(d[key],key)
+        })
+        markers[key] = {'el':el, 
+        'marker': new mapboxgl.Marker(el)
+            .setLngLat(d[key].geometry.coordinates)
+            .addTo(map)
+        }
+    })
 });
