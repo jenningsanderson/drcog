19eb4a86073ac06103a183c5cbce32c6b053ff48
Jennings Anderson <jennings.anderson@colorado.edu>
2018-10-11 12:17:49 -0600

    new map reduce for coverage

--- util/index-coverage.js
+++ util/index-coverage.js
@@ -6,20 +6,24 @@ var tileReduce = require('@mapbox/tile-reduce');
 var _ = require('lodash')
 
 // const mapScript = "map/map-tiger-tags.js"
-const mapScript = "map/map-building-stats.js";
+const mapScript = "map/map-coverage.js";
 
 var output    = "../data/denver-buildings.data"
 
-var users = {}
+var features = []
 
 tileReduce({
     map: path.join(__dirname, mapScript),
     zoom: 12,
-    sources: [{name: 'osm', mbtiles: path.join("/data/planet/latest.planet.mbtiles"), raw: false},
-              {name: 'drcog', mbtiles: path.join(__dirname, "../drcog.mbtiles", raw:false}],
-    bbox: [-105.5598,39.2467,-104.2325,40.6206]
+    sources: [{name: 'osm', mbtiles: path.join("/data/planet/penultimate.planet.mbtiles"), raw: false},
+              {name: 'drcog', mbtiles: path.join(__dirname, "../drcog.mbtiles"), raw:false}],
+    bbox: [-105.5598,39.2467,-104.2325,40.6206],
+    maxWorkers: 8
 })
-.on('reduce', function(res){
+.on('reduce', function(feat){
+    features.push(feat)
 })
 .on('end', function(){
+    var outFile = fs.createWriteStream("../docs/drcog.geojson")
+    outFile.write(JSON.stringify({type:"FeatureCollection",features:features}));
 })
--- util/map/map-coverage.js
+++ util/map/map-coverage.js
@@ -1,93 +1,33 @@
 var _     = require('lodash');
-var topojson = require('topojson');
-var turf = require("@turf/turf")
-
-/*
-*    Reconstruct historical objects (generator style)
-*    [ Could consider making a "smart" feature object that could handle *much* of this? ]
-*/
-function* historyGenerator(historyString){
-    let hT = JSON.parse(historyString) //hT = historical topology
-    let keys   = _.sortBy(Object.keys(hT.objects),function(v){return Number(v)});
-    for (var i=0;i<keys.length; i++){
-        yield topojson.feature(hT,hT.objects[keys[i]]);
-//         yield [topojson.feature(hT,hT.objects[keys[i]]),i,keys.length];
-    }
-    return keys.length
-}
+var turf = require("@turf/turf");
+var tilebelt = require("tilebelt");
 
 module.exports = function(data, tile, writeData, done) {
 
   //Extract the osm layer from the mbtile
-  var layer = data.history.historical_topojson;
-
-  var featCount = 0;
-  var buildings = 0;
-  var buildingHeights = 0;
+  var drcog = data.drcog.polies_4326
+  var osm   = data.osm.osm
     
-  var users = {}
-
-  layer.features.forEach(function(feat){
-
-    featCount++;
-
-    if (feat.properties.building && feat.properties.building != 'no'){
-        
-        buildings++;
-        
-        hG = historyGenerator(feat.properties['@history'])
-            
-        var done, version, vIt, name, thisUser, prevUser
-        while(!done){
-            vIt     = hG.next();
-            version = vIt.value
-            done    = vIt.done
-            
-            if (!done){
-                
-                // Record the edge
-                thisUser = version.properties['@user']
-                if (version.properties['@version']>1){
-                    var key = `${thisUser},${prevUser}`
-                    
-                    if ( users.hasOwnProperty(key) ){
-                        users[key] += 1
-                    }else{
-                        users[key] = 1
-                    }
-                }
-                prevUser = thisUser;
-                
-                
-                //attributes
-                if ( version.properties.hasOwnProperty('aA') ){
-                  
-                  if ( version.properties.aA.hasOwnProperty('name') ){
-                    name = version.properties.aA.name
-                  }
-                }
-                
-//                 if ( version.properties.hasOwnProperty('aM') ){
-//                   if ( version.properties.aM.hasOwnProperty('name') ){
-//                     //
-//                   }
-//                 }
-                
-                //Write the row
-                writeData(`${[
-                    feat.properties['@id'],
-                    version.properties['@user'],
-                    version.properties['@version'],
-                    version.properties['@minorVersion'],
-                    version.properties['@validSince'],
-                    version.properties['@validUntil'],
-                    name
-                          ].join("\t")}\n`)
-            }
-            
-        }
-    }
+  osmBuildings = 0;
+  osm.features.forEach(function(feat){
+      if (feat.properties.building && feat.properties.building!='no'){
+          osmBuildings++
+      }
   });
+  
+  var drCogBuildings = drcog.features.length;
+    
+  var percentageComplete =  osmBuildings / drCogBuildings * 100  
+  var thisTile = {
+      type:"Feature",
+      geometry: tilebelt.tileToGeoJSON(tile),
+      properties: {
+          'drcog' : drCogBuildings,
+          'osm'   : osmBuildings,
+          'percentComplete':Math.round(percentageComplete)
+      }
+  }
+//   writeData(JSON.stringify(thisTile)+"\n")
 
-  done(null, [users,0])
+  done(null, thisTile)
 };
