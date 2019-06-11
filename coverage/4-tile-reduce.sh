7e855efe0786c5219cc065ca723f7a9d0ecba18d
Jennings Anderson <jenningsanderson@colorado.edu>
2019-06-11 16:26:07 -0600

    updating on june 11

--- coverage/4-tile-reduce.sh
+++ coverage/4-tile-reduce.sh
@@ -1,7 +1,7 @@
 # Run tile-reduce to compute the coverage, in accordance to the grids?
 
 echo "running tile-reduce based on the following files: "
-ehco "  1. osm_buidings.mbtiles"
-ehco "  2. ../data/drcogZ15.mbtiles"
+echo "  1. osm_buidings.mbtiles"
+echo "  2. ../data/drcogZ15.mbtiles"
 
-node index.js
+node index-coverage.js
