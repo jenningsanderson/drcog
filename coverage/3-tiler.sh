74408fb88a0e120457066e18155385dbf720ba03
Jennings Anderson <jenningsanderson@colorado.edu>
2019-08-06 15:00:15 -0600

    updating stats for August

--- coverage/3-tiler.sh
+++ coverage/3-tiler.sh
@@ -1,3 +1,3 @@
 echo "Tiling Buildings in OSM"
 
-tippecanoe -o osm_buildings.mbtiles -Z12 -z15 -Pf -pf -pd -ps --no-duplication --no-tile-stats -l buildings all_buildings.geojsonseq
+tippecanoe -o osm_buildings.mbtiles -Z12 -z15 -Pf -pk -pf -pd -ps --no-duplication --no-tile-stats -l buildings all_buildings.geojsonseq
